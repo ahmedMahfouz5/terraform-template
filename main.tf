b251fdccaf453b9ae1584d202eaba71a480155a7
ahmed <ahmedmahfouzt@gmail.com>
2025-07-07 12:16:30 +0300

    Add security group module configuration to main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,10 @@ module "networking" {
   cidr_public_subnet   = var.cidr_public_subnet
   us_availability_zone = var.us_availability_zone
 }
+
+module "security_group" {
+  source              = "./security-groups"
+  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
+  vpc_id              = module.networking.vpc_id
+  ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+}
