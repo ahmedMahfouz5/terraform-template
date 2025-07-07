f2e24ec27a0f822a663e44c62b1ab888deb033a3
ahmed <ahmedmahfouzt@gmail.com>
2025-07-07 12:58:21 +0300

    main.tf vars

--- main.tf
+++ main.tf
@@ -8,19 +8,19 @@ module "networking" {
 
 module "security_group" {
   source              = "./security-groups"
-  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
+  ec2_sg_name_ssh         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
   vpc_id              = module.networking.vpc_id
-  ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+  ec2_sg_name_http = "Allow port 8080 for jenkins"
 }
 
 module "compute" {
   source                    = "./compute"
-  ami_id                    = var.ec2_ami_id
-  instance_type             = "t2.medium"
+  ec2_ami_id                = var.ec2_ami_id
+  instance_type             = var.instance_type
   tag_name                  = "Ubuntu Linux EC2"
   public_key                = var.public_key
   subnet_id                 = tolist(module.networking.public_subnets)[0]
-  sg_for_jenkins            = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_jenkins_port_8080]
+  sg_for_jenkins            = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_port_8080_id]
   enable_public_ip_address  = true
   user_data_install_jenkins = templatefile("./template/template.sh", {})
 }
