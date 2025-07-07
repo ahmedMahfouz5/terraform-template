841f404d0e94ddc080f25bce60356ae57f66ce3c
ahmed <ahmedmahfouzt@gmail.com>
2025-07-07 14:23:35 +0300

    user_data_install name

--- main.tf
+++ main.tf
@@ -22,5 +22,5 @@ module "compute" {
   subnet_id                 = tolist(module.networking.public_subnets)[0]
   sg_for_jenkins            = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_port_8080_id]
   enable_public_ip_address  = true
-  user_data_install_jenkins = templatefile("./template/template.sh", {})
+  user_data_install = templatefile("./template/template.sh", {})
 }
