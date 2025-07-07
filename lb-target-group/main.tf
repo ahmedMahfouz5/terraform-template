b15d985ec4c1e220e4989ca1a76582dd2e9ed7cb
ahmed <ahmedmahfouzt@gmail.com>
2025-07-07 19:07:47 +0300

    aws_lb_target_group_attachment port

--- lb-target-group/main.tf
+++ lb-target-group/main.tf
@@ -27,5 +27,5 @@ resource "aws_lb_target_group" "lb_target_group" {
 resource "aws_lb_target_group_attachment" "lb_target_group_attachment" {
   target_group_arn = aws_lb_target_group.lb_target_group.arn
   target_id        = var.ec2_instance_id
-  port             = 8080
+  port             = 80
 }
